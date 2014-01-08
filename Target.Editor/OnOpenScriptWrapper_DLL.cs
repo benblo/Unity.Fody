2b5b0b3afad8a6404f60c739aba041c3290f024b
benblo <benoit@swingswingsubmarine.com>
2014-01-08 18:10:56 +0100

    add editor project

---
+++ Target.Editor/OnOpenScriptWrapper_DLL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+public static class OnOpenScriptWrapper_DLL
+{
+	[OnOpenAsset]
+	public static bool OnOpenAsset( int _instanceId, int _line )
+	{
+		Debug.Log(string.Format("OnOpenScriptWrapper_DLL {0}, {1}", _instanceId, _line));
+
+		//string path = AssetDatabase.GetAssetPath(_instanceId);
+		//var asset = EditorUtility.InstanceIDToObject(_instanceId);
+
+		return false;
+	}
+}
